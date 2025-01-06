453d6d81ba53118f32639d7bcfde6c4cc76a33e6
cranycrane <xjerab28@stud.fit.vutbr.cz>
2025-01-06 10:41:21 +0100

    fix uniform generator

--- algo/gen-trans1.cpp
+++ algo/gen-trans1.cpp
@@ -9,6 +9,9 @@
 
 double trans() {
     unsigned int x = RND();
-    double vysledek = (x / (double) UINT_MAX + 1) * 8.0 + 2.0;
+    double vysledek = (x / ((double) UINT_MAX + 1.0)) * 8.0 + 2.0;
     return vysledek;
 }
+
+// k UINT davame '+1' protoze kong. gen generuje '0 <= x < m' s rovn. rozložením (viz. opora)
+// bez '+1' by to bylo <2,10> a ne <2,10)
